0d0a1bfa1cedcdff13ff6fb59cf076b124e3680d
Sebastian <sebastian.thorneus@gmail.com>
2022-07-03 02:39:28 +0100

    fixa port på railway

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require('cors');
 
-const port = `0.0.0.0:$mail`;
+const port = `0.0.0.0:$/mail`;
 const app = express();
 
 const emailsRouter = require("./routes/emails.router");
