a09eb65c1d4f44cf538d96044a99cc3938fa6995
Sebastian <sebastian.thorneus@gmail.com>
2022-07-03 02:07:31 +0100

    port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require('cors');
 
-const port = 4000;
+//const port = 4000;
 const app = express();
 
 const emailsRouter = require("./routes/emails.router");
@@ -12,6 +12,6 @@ app.use(bodyParser.json());
 
 app.use(emailsRouter);
 
-app.listen(`0.0.0.0:$PORT`, () => {
-    console.log(`Servern kör på port ${port}`)
+app.listen(`PORT`, () => {
+    console.log(`Servern kör på PORT`)
 });
