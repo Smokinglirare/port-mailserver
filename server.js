cc21b862ace63a47670853e4bf15f1507cf0f7d1
Sebastian <sebastian.thorneus@gmail.com>
2022-08-27 23:35:39 +0100

    adderat nodemailer

--- server.js
+++ server.js
@@ -7,6 +7,33 @@ const app = express();
 
 const emailsRouter = require("./routes/emails.router");
 
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN
+    }
+  });
+
+  let mailOptions = {
+    from: tomerpacific@gmail.com,
+    to: tomerpacific@gmail.com,
+    subject: 'Nodemailer Project',
+    text: 'Hi from your nodemailer project'
+  };
+
+  transporter.sendMail(mailOptions, function(err, data) {
+    if (err) {
+      console.log("Error " + err);
+    } else {
+      console.log("Email sent successfully");
+    }
+  });
+
 app.use(cors());
 app.use(bodyParser.json());
 
