a8ca85b7d752c2dec0d125cff7d7c980ff0ca632
Sebastian <sebastian.thorneus@gmail.com>
2022-08-27 22:04:00 +0100

    redeploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require('cors');
+const cors = require('cors')
 
 const port = process.env.PORT || 4000;
 const app = express();
